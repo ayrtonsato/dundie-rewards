93f1f952c0d6405197d1d61f4da01c231269d093
Ayrton Hiroshi Sato <ayrton_sato@hotmail.com>
2023-02-21 11:48:02 -0300

    Adding finalizers

--- dundie/utils/log.py
+++ dundie/utils/log.py
@@ -14,7 +14,7 @@ def get_logger(logfile="dundie.log"):
     """Returns a configured logger"""
     fh = handlers.RotatingFileHandler(
         logfile,
-        maxBytes=100,  # 10 ** 6
+        maxBytes=1000,  # 10 ** 6
         backupCount=10
     )
     fh.setLevel(LOG_LEVEL)
--- tests/test_load.py
+++ tests/test_load.py
@@ -3,9 +3,30 @@ from dundie.core import load
 from .constants import PEOPLE_FILE
 
 
+def setup_module():
+    print()
+    print("roda antes dos testes desse módulo\n")
+
+
+def teardown_module():
+    print()
+    print("roda após dos testes desse módulo\n")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def create_new_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("isso é sujeira")
+    yield
+    file_.remove()
+
+
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load(request):
     """Test load function"""
+
+    request.addfinalizer(lambda: print("Terminou"))
+
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == "J"
