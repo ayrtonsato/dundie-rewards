4b4207123aee76d4025f664f4b7cbd9335fe361b
Ayrton Sato <ayrton_sato@hotmail.com>
2023-02-21 12:20:39 -0300

    Making an error (#16)

--- tests/test_load.py
+++ tests/test_load.py
@@ -5,7 +5,13 @@ from .constants import PEOPLE_FILE
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_has_2_people():
     """Test load function"""
-    assert len(load(PEOPLE_FILE)) == 2
+    assert len(load(PEOPLE_FILE)) == 3
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_firstname_starts_with_j():
+    """Test load function"""
     assert load(PEOPLE_FILE)[0][0] == "J"
