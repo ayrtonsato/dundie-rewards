06764555b4dc575fc948f7cc044a793f252c99ca
Ayrton Sato <ayrton_sato@hotmail.com>
2023-03-15 20:56:07 -0300

    Get rates (#19)
    * pydantic
    
    * added sqlmodel as dep
    
    * added sql support
    
    * added migrations with alembic
    
    * Add migrations com alembic
    
    * fix lint issue

--- dundie/settings.py
+++ dundie/settings.py
@@ -6,4 +6,10 @@ SMTP_TIMEOUT = 5
 EMAIL_FROM = "master@dundie.com"
 
 ROOT_PATH = os.path.dirname(__file__)
-DATABASE_PATH = os.path.join(ROOT_PATH, "..", "assets", "database.json")
+DATABASE_PATH = os.path.join(ROOT_PATH, "..", "assets", "database.db")
+
+SQL_CON_STRING = f"sqlite:///{DATABASE_PATH}"
+
+DATEFMT = "%d/%m/%Y %H:%M:%S"
+
+API_BASE_URL = "https://economia.awesomeapi.com.br/json/last/USD-{currency}"
